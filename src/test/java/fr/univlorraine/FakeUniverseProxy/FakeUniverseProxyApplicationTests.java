1d45de0d654bf983f340c5f041c34652f032e8dc
kryffin <nicolaskleinhentz@gmail.com>
2020-12-01 16:20:10 +0100

    Initial commit

---
+++ src/test/java/fr/univlorraine/FakeUniverseProxy/FakeUniverseProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univlorraine.FakeUniverseProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeUniverseProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
